e22b754605683626ee1907866ac32c9195e55ebc
Kieran Hislop <kieranjhislop@gmail.com>
2025-10-01 15:13:08 -0300

    added pngs above current gizmos

--- Assets/Scripts/GizmoDrawer.cs
+++ Assets/Scripts/GizmoDrawer.cs
@@ -23,16 +23,19 @@ public class GizmoDrawer : MonoBehaviour
         {
             case GizmoType.SpawnPoint:
                 Gizmos.color = Color.green;
+                Gizmos.DrawIcon(transform.position + new Vector3(0f, 1.0f, 0f), "heart.png", true);
                 Gizmos.DrawCube(transform.position, Vector3.one * 0.5f);
                 break;
 
             case GizmoType.Waypoint:
                 Gizmos.color = Color.blue;
+                Gizmos.DrawIcon(transform.position + new Vector3(0f, 1.0f, 0f), "waypoint.png", true);
                 Gizmos.DrawWireSphere(transform.position, radius);
                 break;
 
             case GizmoType.Enemy:
                 Gizmos.color = Color.red;
+                Gizmos.DrawIcon(transform.position + new Vector3(0f, 1.0f, 0f), "skull.png", true);
                 Gizmos.DrawWireCube(transform.position, Vector3.one);
                 break;
         }         
@@ -44,11 +47,13 @@ public class GizmoDrawer : MonoBehaviour
         {
             case GizmoType.TriggerZone:
                 Gizmos.color = new Color(1f, 1f, 0f, 0.3f);
+                Gizmos.DrawIcon(transform.position + new Vector3(0f, 1.0f, 0f), "Trigger.png", true);
                 Gizmos.DrawCube(transform.position, size);
                 break;
 
             case GizmoType.Player:
                 Gizmos.color = Color.cyan;
+                Gizmos.DrawIcon(transform.position + new Vector3(0f, 1.0f, 0f), "player.png", true);
                 Gizmos.DrawSphere(transform.position, radius);
                 break;
         }
